62c3c1dd4dead0ba7a380182b64aad30f93d8d32
distinct-hyena <43580779+distinct-hyena@users.noreply.github.com>
2018-10-31 17:37:08 -0400

    Revert "added /hello route to hello world page"
    This reverts commit 0ac38673a473c805faf02b17ca38381ba1171fb1.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,7 @@
 const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
-const helloControllers = require('../controllers/hello.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
-router.get('/hello', helloControllers.hello);
 
 module.exports = router;
