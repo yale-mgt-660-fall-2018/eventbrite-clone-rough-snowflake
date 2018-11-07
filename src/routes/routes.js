0842943dbb5b4ee709e168b8c61d0841f090a733
megan ananian <megan.ananian@yale.edu>
2018-11-07 18:47:49 +0000

    added about us route to routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
 const indexControllers = require('../controllers/index.js');
 const helloControllers = require('../controllers/hello.js');
+const aboutControllers = require('../controllers/about.js');
 
 const router = new Router();
 router.get('/', indexControllers.index);
 router.get('/hello', helloControllers.hello);
+router.get('/about', aboutControllers.about);
 
 module.exports = router;
