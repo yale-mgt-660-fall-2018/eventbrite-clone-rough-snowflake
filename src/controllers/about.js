964b800f36ea5b85685756ac85d7b995c15411e4
megan ananian <megan.ananian@yale.edu>
2018-11-14 19:50:07 +0000

    megan week 2

--- src/controllers/about.js
+++ src/controllers/about.js
@@ -7,6 +7,11 @@ async function about(ctx) {
     return ctx.render(template, { });
 }
 
+function about (request, response) {
+    console.log('in about function');
+    
+    nicknames = distinct-hyena, fair-barracuda, healthy-reindeer, cheerful-penguin
+
 module.exports = {
     about,
 };
