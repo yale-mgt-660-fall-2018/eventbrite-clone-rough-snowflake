962ba0e18990424877b50c5d3b599ae75c74bfb0
Sami Boghos <sami.boghos@yale.edu>
2018-11-14 00:09:20 +0000

    error logging

--- src/controllers/newEvent.js
+++ src/controllers/newEvent.js
@@ -12,8 +12,14 @@ async function index(ctx) {
 }
 
 async function index1(ctx) {
-   
+    const r = {};
+    try{
     const r = await eventsModel.insert(ctx.db,ctx.request.body.title,ctx.request.body.date,ctx.request.body.image,ctx.request.body.location);
+    } catch(e){
+        const template = 'newEvent.njk';
+        analyticsModel.getSessionId(ctx, "new_event");
+        return ctx.render(template, { "error": "There was an error in your form" });
+    }
     analyticsModel.getSessionId(ctx, "create_event");
     ctx.redirect('/events/'+(r.id));
 }
