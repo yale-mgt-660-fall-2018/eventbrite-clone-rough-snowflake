38b96ddb492a3c1a0ed0aa19c4c350b3b1b6144b
Sami Boghos <sami.boghos@yale.edu>
2018-11-15 01:03:15 +0000

    fixed issues with error logging

--- src/models/schema.sql
+++ src/models/schema.sql
@@ -2,7 +2,7 @@
 --DROP EXTENSION IF EXISTS pgcrypto;
 --CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-DROP TABLE IF EXISTS events CASCADE;
+--DROP TABLE IF EXISTS events CASCADE;
 CREATE TABLE IF NOT EXISTS events (
     -- Integer primary key for events
     id serial PRIMARY KEY,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS events (
     "date" TIMESTAMP WITH TIME ZONE NOT NULL,
     -- The 'image_url' must be a URL ending in png, gif.
     image_url TEXT NOT NULL
-        CHECK ( image_url ~ '^https?://.*\.(png|gif|jpg)$' ),
+        CHECK ( image_url ~* '^https?://.*\.(png|gif|jpg)$' ),
     "location" TEXT NOT NULL,
     -- Record the time at which this event was created
     created_at TIMESTAMP WITH TIME ZONE
@@ -21,22 +21,21 @@ CREATE TABLE IF NOT EXISTS events (
         DEFAULT current_timestamp
 );
 
-DROP TABLE IF EXISTS attendees CASCADE;
+--DROP TABLE IF EXISTS attendees CASCADE;
 CREATE TABLE IF NOT EXISTS attendees (
     event_id INT NOT NULL,
     email TEXT
-     CHECK (email ~ '%*@yale.edu' ),
+     CHECK (email ~* '%*@yale.edu' ),
     UNIQUE(event_id,email)
 );
 
-DROP TABLE IF EXISTS users CASCADE;
+--DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users (
     id serial PRIMARY KEY,
     referer TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS pageviews CASCADE;
-DROP TABLE IF EXISTS views CASCADE;
+--DROP TABLE IF EXISTS pageviews CASCADE;
 CREATE TABLE IF NOT EXISTS pageviews (
     user_id INT NOT NULL,
     p TEXT NOT NULL
